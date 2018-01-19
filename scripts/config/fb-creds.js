b0fa1c25496de2640acee5878f0fb38b2183e2ce
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-19 14:49:28 -0600

    add keys to git ignore

---
+++ scripts/config/fb-creds.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = {
+    fbKey: `AIzaSyCKGHFjMy19DPwRAm2I8kk6VxEcKEcEhgo`,
+    authDomain:`magic-spinning-bugs.firebaseio.com/`
+}
---
+++ scripts/config/movieKey.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = {
+    apiMovieKey: `768adb7bd8b393bfcc5436336d402f3f`,
+}
